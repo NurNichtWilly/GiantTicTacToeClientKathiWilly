32365da63f0231fb797327091d6ed79a66fcd406
Willy Menzel <willmenz18@computercamp.local>
2018-08-21 12:10:01 +0200

    Simons Briefing

---
+++ client.py
@@ -0,0 +1,33 @@
+
+class 
+class Wsdf():
+	x = 5
+	
+	def __init__(self):
+		# konstruktor
+	
+	@staticmethod
+	def __str__():
+	
+	def yxcv(self, bar):
+		return bar
+	
+	def qwer(self, foo):
+		self.x = 5
+		test = self.yxcv(self.x)
+		return (test, foo)
+		
+
+uiae = asdf()
+print(uiae)
+x, y = uiae.qwer("uio")
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DATAGRAM)
+socket.recv
+socket.send
+
+'test\x00string'.encode('utf-8')
+
+if __name__ == '__main__':
+	pass
+
